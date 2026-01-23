d230d77ac8a13c6e1a3e372368287a7f13e347c1
nguyenhongquan <nguyenhongquan.work@gmail.com>
2026-01-24 01:22:57 +0700

    fix: deploy render

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,17 +1,28 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined
+  // eslint-disable-next-line no-var
+  var __prismaPool: Pool | undefined
 }
+
+const pool =
+  global.__prismaPool ??
+  new Pool({ connectionString: process.env.DATABASE_URL })
+
+const adapter = new PrismaPg(pool)
 
 const prisma =
   global.__prisma ??
-  new PrismaClient()
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') {
   global.__prisma = prisma
+  global.__prismaPool = pool
 }
 
 export default prisma
